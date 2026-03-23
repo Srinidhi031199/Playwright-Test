c47a088452e52e13d38d0236f9563d8bdc7d1046
Srinidhi.S <srinidhishankar09@gmail.com>
2026-03-23 15:56:52 +0530

    Add callback examples

---
+++ JS_Notes/Chapter-9-Functions/Chapter-23-CallBack-me.js
@@ -0,0 +1,14 @@
+function Sri_doing_work(worker, callback) {
+    console.log("Started the class PW")
+    let work = worker;
+
+    // dasdasdasdsadasdsadas
+    console.log("Finished the class PW")
+    callback();
+}
+
+function callWife() {
+    console.log("Call wife when done");
+}
+
+Sri_doing_work('PW class', callWife);
